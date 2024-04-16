55b910a25e7bab21c531853b3fa2b4a86bc4fde7
frosch <frosch@openttd.org>
2024-04-16 20:30:41 +0200

    feat: sanitize_whitespace

--- src/validate.rs
+++ src/validate.rs
@@ -1,3 +1,4 @@
+use crate::parser::{FragmentContent, ParsedString, StringCommand};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug)]
@@ -32,11 +33,42 @@ pub fn validate(
     None
 }
 
-//fn sanitize_string(parsed: &mut ParsedString) {
-// remove trailing white-space
-// remove whitespace in front of {}
-// remove control chars
-//}
+fn remove_ascii_ctrl(t: &mut String) {
+    *t = t.replace(|c| char::is_ascii_control(&c), " ");
+}
+
+fn remove_trailing_blanks(t: &mut String) {
+    if let Some(last) = t.rfind(|c| c != ' ') {
+        t.truncate(last + 1);
+    }
+}
+
+/// Replace all ASCII control codes with blank.
+/// Remove trailing blanks at end of each line.
+fn sanitize_whitespace(parsed: &mut ParsedString) {
+    let mut is_eol = true;
+    for i in (0..parsed.fragments.len()).rev() {
+        let mut is_nl = false;
+        match &mut parsed.fragments[i].content {
+            FragmentContent::Text(t) => {
+                remove_ascii_ctrl(t);
+                if is_eol {
+                    remove_trailing_blanks(t);
+                }
+            }
+            FragmentContent::Choice(c) => {
+                for t in &mut c.choices {
+                    remove_ascii_ctrl(t);
+                }
+            }
+            FragmentContent::Command(c) => {
+                is_nl = c.name.is_empty();
+            }
+            _ => (),
+        }
+        is_eol = is_nl;
+    }
+}
 
 //fn check_string(parsed: &mut ParsedString) {
 // project-type, language-info, base-language
@@ -59,3 +91,27 @@ pub fn validate(
 // unimportant amount, but > 0
 // mode-params for important parameters
 //}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sanitize() {
+        let mut s1 = String::from("");
+        let mut s2 = String::from(" a b c ");
+        let mut s3 = String::from("\0a\tb\rc\r\n");
+        remove_ascii_ctrl(&mut s1);
+        remove_ascii_ctrl(&mut s2);
+        remove_ascii_ctrl(&mut s3);
+        assert_eq!(s1, String::from(""));
+        assert_eq!(s2, String::from(" a b c "));
+        assert_eq!(s3, String::from(" a b c  "));
+        remove_trailing_blanks(&mut s1);
+        remove_trailing_blanks(&mut s2);
+        remove_trailing_blanks(&mut s3);
+        assert_eq!(s1, String::from(""));
+        assert_eq!(s2, String::from(" a b c"));
+        assert_eq!(s3, String::from(" a b c"));
+    }
+}
